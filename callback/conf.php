2a04734473fa04bb2f5600171125df52d3348bf8
antspriggs <perspectiveva@gmail.com>
2016-12-13 10:28:15 -0500

    Revert "Initialize Repo"
    This reverts commit 9854fa85fe395718de66ddad411527dc8b47f695.

--- callback/conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-  define("CLIENT_ID", "25d442c9d4ecc7ef01a3e41577caebb7");
-  define("CLIENT_SECRET", "5dba67ecc78f5bd5c8a3da035fe59f58");
-  define("REDIRECT_URI", "http://localhost:3000/callback");
-  define("AUTH_URL", "https://api.id.me/oauth/authorize");
-  define("TOKEN_URL", "https://api.id.me/oauth/token");
-  define("ATTRIBUTES_URL", "https://api.id.me/api/public/v2/attributes.json");
-?>
